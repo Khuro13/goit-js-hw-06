f86937116ebec5b630872f63dd8bf48b77aa170e
Ivan Dobrolyubov <66012866+Khuro13@users.noreply.github.com>
2020-09-21 15:46:09 +0300

    Update task.js

--- js/task.js
+++ js/task.js
@@ -1,47 +1,47 @@
 import users from './users.js';
 
-// Задание 1
+// task 1
 const getUserNames = users => users.map(user => user.name);
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-// Задание 2
+// task 2
 const getUsersWithEyeColor = (users, color) =>
   users.filter(user => user.eyeColor === color);
 
 console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-// Задание 3
+// task 3
 const getUsersWithGender = (users, gender) => {
   const maleGender = users.filter(user => user.gender === gender);
   return maleGender.map(user => user.name);
 };
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-// Задание 4
+// task 4
 const getInactiveUsers = users => users.filter(user => user.isActive);
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-// Задание 5
+// task 5
 const getUserWithEmail = (users, email) =>
   users.find(user => user.email === email);
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-// Задание 6
+// task 6
 const getUsersWithAge = (users, min, max) =>
   users.filter(user => min <= user.age && user.age <= max);
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-// Задание 7
+// task 7
 
 const calculateTotalBalance = users =>
   users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
 console.log(calculateTotalBalance(users)); // 20916
 
-// Задание 8
+// task 8
 
 const getUsersWithFriend = (users, friendName) =>
   users
@@ -50,7 +50,7 @@ const getUsersWithFriend = (users, friendName) =>
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// Задание 9
+// task 9
 
 const getNamesSortedByFriendsCount = users =>
   users
@@ -59,7 +59,7 @@ const getNamesSortedByFriendsCount = users =>
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-// Задание 10
+// task 10
 
 const getSortedUniqueSkills = users =>
   users
